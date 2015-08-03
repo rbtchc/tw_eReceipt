c2ad9b7468af23cb256f5225cdb617a7cf58bffe
robert.ch.chou <robert.ch.chou@acer.com>
2015-08-03 17:58:15 +0800

    Alter status as valid, type boolean and vat_no as type integer

--- parser.py
+++ parser.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+# -*- coding: utf-8 -*-
 import json
 import os
 import sys
@@ -13,7 +13,7 @@ Type: (1) M: metadata (2) D: detail
 --
 (1) Metadata format: M,status,serial,date,store VAT number,store name,card name,card serial,total amount
     - M: indicates it's metadata
-    - status: open or invalidate
+    - valid: valid or invalidate
     - serial: serial number of the receipt
     - date: receipt date
     - store VAT: value added tax number (or unified biz number)
@@ -31,13 +31,13 @@ Type: (1) M: metadata (2) D: detail
 data = {}
 with open(sys.argv[1], 'r') as csv:
     for l in csv:
-        d = l.decode('big5').encode('utf8').split('|')
+        d = l.decode('big5').split('|')
         if d[0] == 'M':
             data[d[2]] = {
                 'metadata': {
-                    'status':       d[1],
+                    'valid':        True if d[1] == u"\u958b\u7acb" else False, #if d[1] == 開立
                     'date':         d[3],
-                    'vat_no':       d[4],
+                    'vat_no':       int(d[4]),
                     'store_name':   d[5],
                     'card_name':    d[6],
                     'card_serial':  d[7],
@@ -47,6 +47,5 @@ with open(sys.argv[1], 'r') as csv:
         elif d[0] == 'D':
             data[d[1]].setdefault('detail', []).append({'amount': float(d[2]), 'item_name': d[3]})
 
-#print json.dumps(data[data.keys()[0]], indent=4)
 print json.dumps(data, indent=4)
 
